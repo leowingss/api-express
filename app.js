7579146a16ee15e5f3eea54947b76e34274878cd
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-03-11 12:56:01 -0300

    criando receita html

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express")
 const app = express()
-app.get("/", (req,res) => res.send("Olá"))
+app.use(express.json())
+
+app.use(express.static('./public'))
 app.listen(2000, () => console.log("Servidor rodando!"))
