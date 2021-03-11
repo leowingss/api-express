e6053201de9428b7e1a13b30720d1af683de30c5
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-03-10 22:56:08 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Testando")
+const express = require("express")
+const app = express()
+app.get("/", (req,res) => res.send("Olá"))
+app.listen(2000, () => console.log("Servidor rodando!"))
